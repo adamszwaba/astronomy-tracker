72e1927da138a57dee6115f1c3285ce99c24cf23
Adam Szwaba <szwaba.adam@gmail.com>
2024-05-04 12:23:56 +0200

    remove docker stuff

--- backend/main.go
+++ backend/main.go
@@ -8,7 +8,12 @@ import (
 func main() {
 	// create a simple server
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, World!")
+		fmt.Fprintf(w, "hi, World!")
 	})
+
+	http.HandleFunc("/v1/", func(w http.ResponseWriter, r * http.Request) {
+		fmt.Fprintf(w, "maybe")
+	})
+
 	http.ListenAndServe(":8080", nil)
 }
